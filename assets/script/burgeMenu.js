e9d639754a3fcc5224bf01681485bdff85e69b2b
evgenii <vovk.evgenii.199811@gmail.com>
2025-03-28 15:51:42 +0300

    работа над header hero

--- assets/script/burgeMenu.js
+++ assets/script/burgeMenu.js
@@ -1,5 +1,5 @@
 const burgerMenu = document.querySelector(".burger__menu");
-const headerMenu = document.querySelector(".header__menu");
+const headerMenu = document.querySelector(".header__list");
 
 burgerMenu.addEventListener("click", () => {
     headerMenu.classList.toggle("header__menu-checked");
